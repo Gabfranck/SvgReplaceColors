08e303c368f00da1d05184f0f4f81a4edb5b45d9
Gabfranck <gabriel.franck@etu.upmc.fr>
2014-07-15 16:00:39 +0200

    release for Folder management

--- script.sh
+++ script.sh
@@ -4,45 +4,89 @@
 #init fichier source
 for last; do true; done
 
+let "y = $# - 1"
+
+for ((j=0;j<y;j++))
+do
+	color[j]=${1}
+	shift
+done
+
 if [ $# = 0 ]
 then
 		echo "-----------------------------DOC------------------------------"
-		echo "\$last : the file or folder"
-		echo "\$2 : color in the file you want replace"
-		echo "\$3 : replacement color of the previous parameters"
-		echo "\$4 & \$5 : another couple for color replacement"
+		echo "\$last : a svg file or a folder with svgs inside"
+		echo "\$1 : color in the file you want replace"
+		echo "\$2 : replacement color of the previous parameters"
+		echo "\$3 & \$4 : another couple for color replacement"
 		echo "You can repeat the previous step as many times as you want"
 
 elif [ -f $last ]
 then
 		#test pour voir si couple (couleur/remplacement)
-		let "k = $# % 2"
+		let "k = ${#color[@]} % 2"
 
-		if [ $# = 1 ]
+		if [ ${#color[@]} = 0 ]
 		then
-				echo "Colors in the file : "
-				#grep search color (à faire)
+				echo "Colors in the file ${last} :"
+				cat $last | grep "style\s*=\s*\"fill:#......" | sed -e 's/.*style\s*=\s*"fill:#\(......\).*/\1/' | sort -u
 
-		elif [ $k = 1 ]
+		elif [ $k = 0 ]
 		then
 				#premiere passe pour création du fichier _new
-				sed "s/style=\"fill:\#${1};\"/style=\"fill:\#${2};\"/g" $last > ${last%.*}_new.svg
-				shift 2
+				sed "s/style=\"fill:\#${color[0]};\"/style=\"fill:\#${color[1]};\"/g" $last > ${last%.*}_new.svg
 
-				#pour ignorer le fichier source
-				let "x = $# - 1"
-
-				#les autre passe où j'édite le fichier créer
-				for ((i=0;i<x;i=i+2))
+				#les autres passes où j'édite le fichier créé
+				for ((i=2;i<y;i=i+2))
 				do
-						sed -i "s/style=\"fill:\#${1};\"/style=\"fill:\#${2};\"/g" ${last%.*}_new.svg
-						shift 2
+						let "v = $i + 1"
+						sed -i.bak "s/style=\"fill:\#${color[$i]};\"/style=\"fill:\#${color[$v]};\"/g" ${last%.*}_new.svg
 				done
 		else
 				#message d'erreur
-				echo "Make couple of color in \$2 and \$3"
+				echo "Make couple of color in \$1 and \$2"
 		fi
+
+		rm *.bak
+
+elif [ -d $last ]
+then
+		mkdir "${last}/newSvgs"
+		directory="${last}/newSvgs"
+
+		find $last -type f -iname '*.svg' | while read FILE; do
+
+				#test pour voir si couple (couleur/remplacement)
+				let "k = ${#color[@]} % 2"
+
+				if [ ${#color[@]} = 0 ]
+				then
+
+						echo "Colors in the file ${FILE} :"
+						cat ${FILE} | grep "style\s*=\s*\"fill:#......" | sed -e 's/.*style\s*=\s*"fill:#\(......\).*/\1/' | sort -u
+
+				elif [ $k = 0 ]
+				then
+						#premiere passe pour création du fichier _new
+						realName=$(basename ${FILE})
+						sed "s/style=\"fill:\#${color[1]};\"/style=\"fill:\#${color[2]};\"/g" ${FILE} > $directory/${realName%.*}_new.svg
+
+						#les autres passes où j'édite le fichier créé
+						for ((i=0;i<y;i=i+2))
+						do
+								let "v = $i + 1"
+								sed -i.bak "s/style=\"fill:\#${color[$i]};\"/style=\"fill:\#${color[$v]};\"/g" $directory/${realName%.*}_new.svg
+						done
+				else
+						#message d'erreur
+						echo "Make couple of color in \$1 and \$2"
+				fi
+		done
+
+		cd $directory
+		rm *.bak
+
 else
 		#message d'erreur
-		echo "Enter a file in \$last"
+		echo "Enter a file or a directory in \$last"
 fi
